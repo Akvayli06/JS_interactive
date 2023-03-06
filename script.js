779d4adf4249cfcc7d35588c776e52daf7349f44
Akvayli06 <benidirifaouzi@gmail.com>
2023-03-06 19:23:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+// premier_exo
+// const bouton = document.querySelector('footer');
+// bouton.addEventListener('click', function() {
+//     console.log('clique!');
+//   });
+
+// premier_exo_bis
+//   const boutonCount = document.querySelector('footer');
+// boutonCount.addEventListener('click', function() {
+//     console.count('clique!');
+//   });
+
+
+// deuxieme_exo
+// document.querySelector('.navbar-toggler').onclick = function() {myFunction()};
+// function myFunction() {
+//   document.querySelector("#navbarHeader").classList.toggle('collapse');
+// }
+
+// troixieme_exo
+
+// const bouton = document.querySelector(".btn-outline-secondary");
+// const card_text = document.querySelector('p.card-text');
+
+
+// bouton.addEventListener('click', function() {
+//   card_text.style.color = 'red';
+// });
+
+
+// quatrieme_exo
+// const bouton2 = document.querySelectorAll(".card .btn-outline-secondary")[1];
+// const cardText2 = document.querySelectorAll('.card p.card-text')[1];
+
+// bouton2.addEventListener('click', function() {
+//   if (cardText2.style.color === 'green'){
+
+//       cardText2.style.color = '' ;
+      
+//       } else {
+//         cardText2.style.color = 'green';
+//       }
+// });
+
+
+// cinquieme_exo
+// const link = document.querySelector('link[href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"]');
+
+// document.querySelector('.navbar').addEventListener('dblclick', function() {
+//   if (link.disabled){
+
+//       link.disabled = false; 
+      
+//       } else {
+//         link.disabled = true;
+//       }
+// });
+
